5ee23cd9942b1d6652f17e92b188f1a071afbbe1
Kevin Lint <kevin@kevinlint.com>
2015-03-28 13:36:23 -0700

    fix double click

--- app/pages/ui.js
+++ app/pages/ui.js
@@ -9,6 +9,7 @@ UiPage.registerEvents = function(){
     this.dom.logoMini  = '.logo-mini';
    
     // element events
+    container.off();
     container.on('click', this.dom.logo + ', ' + this.dom.logoMini, this.toggleMenu);
 
 };
